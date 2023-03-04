bd6391925000abf57052b16a138f726283bb5862
gustavomarim <gustavo.dantas.marim@outlook.com>
2023-03-04 19:40:09 -0300

    feat(app.tsx): adiciona sessão de header do site

---
+++ src/components/template/Header/index.tsx
@@ -0,0 +1,22 @@
+import { VtexLogo } from '../../icons';
+import Categories from './Categories/index';
+import './Header.sass';
+import Input from './Input';
+import PurchaseInformation from './PurchaseInformation/index';
+import UserNav from './UserNavigation/index';
+
+const Header = () => {
+  return (
+    <header>
+      <PurchaseInformation />
+      <div className='navigation'>
+        <VtexLogo />
+        <Input placeholder='O que você está buscando?' />
+        <UserNav />
+      </div>
+      <Categories />
+    </header>
+  );
+};
+
+export default Header;
