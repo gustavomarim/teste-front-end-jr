20b93452d94492d2236d170220112bcdbbe2deac
gustavomarim <gustavo.dantas.marim@outlook.com>
2023-03-04 20:24:46 -0300

    feat(components): adiciona card de produto exibido no modal

---
+++ src/components/Product/ModalProductCard/index.tsx
@@ -0,0 +1,42 @@
+import { Product } from '../../../core/models/product';
+import { CloseIcon } from '../../icons';
+import './ModalProductCard.sass';
+
+export interface ModalProductCardProps {
+  product: Product[];
+  setOpenModal: () => void;
+}
+
+export const ModalProductCard = (props: ModalProductCardProps) => {
+  return (
+    <>
+      {props.product
+        ? props.product.map((product) => (
+            <div
+              id={product.productName}
+              key={product.productName}
+              className='modal-card'
+            >
+              <div className='product-image'>
+                <img src={product.photo} alt={product.productName} />
+              </div>
+
+              <div className='product-info'>
+                <h2 className='product-name'>{product.productName}</h2>
+                <p className='product-price'>{product.price}</p>
+                <p className='product-description'>
+                  {product.descriptionShort}
+                </p>
+
+                <a href='#'>{'Veja mais detalhes do produto >'}</a>
+              </div>
+
+              <button onClick={props.setOpenModal}>
+                <CloseIcon />
+              </button>
+            </div>
+          ))
+        : null}
+    </>
+  );
+};
