b9e5064ce12ba424a5d744796420c54e39f6485e
gustavomarim <gustavo.dantas.marim@outlook.com>
2023-03-04 20:50:03 -0300

    feat(components/layout): adiciona flexbox layout Este commit adiciona um template responsável por renderizar um layout de grade flexbox, podendo receber parâmetros de customização.

---
+++ src/components/layout/Flex/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Flex.sass';
+
+export interface FlexProps {
+  direction?: 'row' | 'column';
+  gap?: number;
+  padding?: string;
+  wrap?: 'wrap' | 'nowrap';
+  children?: React.ReactNode;
+}
+
+export const Flex = (props: FlexProps) => {
+  const style = {
+    display: 'flex',
+    flexDirection: props.direction,
+    gap: `${props.gap}px`,
+    flexWrap: props.wrap,
+    padding: props.padding,
+  };
+
+  return <div style={style}>{props.children}</div>;
+};
