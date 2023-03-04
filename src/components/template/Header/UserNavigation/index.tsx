475c19e8a7b19a084606031afbc364f5ba4c8d6b
gustavomarim <gustavo.dantas.marim@outlook.com>
2023-03-04 19:29:17 -0300

    feat(components/header): adiciona componente de navegação de usuário Este commit adiciona o componente gráfico que contém a navegação do usuário em sua conta, como o carrinho de compras, itens favoritos, perfil e um input de texto referente ao filtro de pesquisa de produtos.

---
+++ src/components/template/Header/UserNavigation/index.tsx
@@ -0,0 +1,26 @@
+import { Group, Heart, ShoppingCart, UserCircle } from '../../../icons';
+import './UserNavigation.sass';
+
+const UserNavigation = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Group />
+        </li>
+        <li>
+          <Heart />
+        </li>
+        <li>
+          <UserCircle />
+        </li>
+        <li>
+          {' '}
+          <ShoppingCart />
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default UserNavigation;
