46c37aca8d87845d2f1535e24b3cbb34f20fe919
gustavomarim <gustavo.dantas.marim@outlook.com>
2023-03-05 11:30:20 -0300

    feat(components): adiciona card de marcas

---
+++ src/components/Brand/BrandCard/index.tsx
@@ -0,0 +1,10 @@
+import { VtexLogo } from '../../icons';
+import './BrandCard.sass';
+
+export const BrandCard = () => {
+  return (
+    <div className='brand-card'>
+      <VtexLogo />
+    </div>
+  );
+};
